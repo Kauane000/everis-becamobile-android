531b3a9bb6453f55001a5a344698c0786146737b
kauane silva <kauane.nep@gmail.com>
2021-03-31 13:23:18 -0300

    Instanciando classes de constutor na interface da api

--- GeekFlix/app/src/main/java/com/example/geekflix/api/FilmesApi.kt
+++ GeekFlix/app/src/main/java/com/example/geekflix/api/FilmesApi.kt
@@ -1,6 +1,8 @@
 package com.example.geekflix.api
 
 
+import com.example.geekflix.model.Filme
+import com.example.geekflix.model.FilmesTask
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,10 +13,10 @@ interface FilmesApi {
 
 
     @GET("trending/movie/week")
-    fun getAllList(@Query("api_key") api:String): Call<>
+    fun getAllList(@Query("api_key") api:String): Call<FilmesTask>
 
     @GET("movie/{id}")
-    fun getAllListDetales(@Path("id") id: String, @Query("api_key") api:String): Call<>
+    fun getAllListDetales(@Path("id") id: String, @Query("api_key") api:String): Call<Filme>
 
 }
 
