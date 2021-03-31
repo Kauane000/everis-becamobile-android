5d5fd0c5a7f7a3979a0f48d07880d7f0721f11f2
kauane silva <kauane.nep@gmail.com>
2021-03-31 14:04:34 -0300

    implementação da class viewMolde e adição de observer na main para exibir o que for view para a main

--- GeekFlix/app/src/main/java/com/example/geekflix/view/MainActivity.kt
+++ GeekFlix/app/src/main/java/com/example/geekflix/view/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.geekflix.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.geekflix.R
+import com.example.geekflix.model.FilmesTask
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val viewModel: MainViewModel by viewModels()
+        viewModel.callListFilme().observe(this, Observer {
+            setAdapeter(it)
+        }
+        )
     }
+
+    private fun setAdapeter(filmesTask: FilmesTask?) {
+        recyclerList.layoutManager = LinearLayoutManager(this@MainActivity)
+        recyclerList.adapter = filmesTask?.let { ListFilmesAdapter(it.returnList, this) }
+    }
+
 }
--- GeekFlix/app/src/main/java/com/example/geekflix/view/MainViewModel.kt
+++ GeekFlix/app/src/main/java/com/example/geekflix/view/MainViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.geekflix.view
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.geekflix.api.RestApi
+import com.example.geekflix.model.FilmesTask
+import retrofit2.Call
+import retrofit2.Response
+
+class MainViewModel : ViewModel(){
+    private val filmesLiveData : MutableLiveData<FilmesTask> = MutableLiveData()
+
+    fun callListFilme(): MutableLiveData<FilmesTask> {
+        val call = RestApi.retrofitApi().getAllList("579dbbdd2de6dd3cc42c4d65dc3afdae")
+
+        call.enqueue(object : retrofit2.Callback<FilmesTask> {
+            override fun onResponse(call: Call<FilmesTask>, response: Response<FilmesTask>) {
+                if(response.isSuccessful){
+                    filmesLiveData.value = response.body()
+                }
+            }
+
+            override fun onFailure(call: Call<FilmesTask>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
+
+        return filmesLiveData
+
+    }
+
 }
